a6d8ec075fdf5922f76a87a7d709306cf6d0ab9e
mmurdock1 <102691074+mmurdock1@users.noreply.github.com>
2022-04-13 18:04:29 -0400

    Add files via upload

---
+++ Gate Source Code/add2Hor.m
@@ -0,0 +1,44 @@
+function out = add2Hor(a,b)
+%% Version and general code information
+% Horizontal Addition Updated 2022-04-07
+% Inputs 
+    % Two 3D arrays
+% Outputs
+    %3D array
+    %First 2D array (out(:,:,1)) contains the pattern.
+    %last element (out(:,:,2)) contains the flow rate, should be a
+    %constant.
+
+%% Determining preliminary variables
+resolution = size(a,1);  %Calculates resolution based off the first dimension (# of rows) of a.
+numMatrices = nargin; %Determines the number of matrices based off of the number of input arguments, two.
+
+allMat = nan(resolution,resolution, 2*numMatrices); % Determines the total 3D array size based off the provided resolution and number of matrices. For this case the dimensions are (resolution, resolution, 4).
+allMat(:,:, 1:2) = a; %Assigns all the rows and columns of the 1st and 2nd third dimension of the supplied a array to allMat. The 1st and 2nd will contain the pattern and flow rate arrays of a respectively.
+allMat(:,:, 3:4) = b; %Assigns all the rows and columns of the 1st and 2nd third dimension of the supplied b array to allMat. The 3rd and 4th will contain the pattern and flow rate arrays of b respectively.
+
+%% Flow rate calculation
+flowRate = nan(numMatrices,1); %Create an empty vector that has numMatrices in rows (2) and 1 column.
+
+for i = 1:numMatrices % Loop from 1 to the final number of matrices 2.
+    flowRate(i) = allMat(1,1,2*i); % Obtains the flow rate of each entering stream. Upon each loop iteration "grab" the 1st row, 1st column, and 2*i position in the array and assign it to the i'th spot in the flow rate vector. 
+end
+
+totalFlowRate = sum(flowRate); % Calculates the total flowrate by adding all entries in the flow rate vector.
+
+%% Constructing the combined pattern matrix
+reduceA = imresize(a(:,:,1),[resolution,resolution*flowRate(1)/totalFlowRate],'nearest'); % Take the pattern a(:,:,1) and resize into the specified dimensions based of the fraction of total flow rate (rows) and resolution (columns).  Weights each of the matrices based on their respective flow rates (flowrate(1)).  
+reduceB = imresize(b(:,:,1),[resolution,resolution*flowRate(2)/totalFlowRate],'nearest'); % Take the pattern b(:,:,1) and resize into the specified dimensions based of the fraction of total flow rate (rows) and resolution (columns).  Weights each of the matrices based on their respective flow rates (flowrate(2)). 
+
+addMatrix = [reduceB, reduceA]; %Horizontally concatenates the matrices together. The point of view is from the perspective of the extrudate.
+addMatrix = imresize(addMatrix,[resolution, resolution],'nearest'); %Resize the image to the desired resolution dimensions.
+
+%% Predifining output variables
+out = a; %Assigns the correct dimensions for the output variable.
+out(:,:,1) = addMatrix; %Assigns the added pattern array to the output variable.
+out(:,:,2) = totalFlowRate.*ones(resolution,resolution); %Defines the flow rate 2D array out(:,:,2). Calculates the new flow rate array using the total flow rate multiplied by a ones matrix of size based on the supplied resolution.
+
+
+
+
+
---
+++ Gate Source Code/cutHor.m
@@ -0,0 +1,43 @@
+function [outA, outB] = cutHor(a)
+%% Version and general code information
+%Cut Horizontal V 1.0 Updated 2022-04-07
+% Inputs 
+    % One 3D array
+% Outputs
+    %Two 3D arrays
+    %First 2D array (out(:,:,1)) contains the pattern.
+    %last element contains the flow rate (out(:,:,2)), should be a
+    %constant.
+
+%% Determining preliminary variables
+resolution = size(a,1); %Calculates resolution based off of the first dimension (# of rows) of a.
+numMatrices = nargin; %Calculates the number of matrices based off  the supplied input arguments, one. 
+numOutMatrices = nargout;  %Determines the number of output matrices based off the number of output arguments,two.
+
+%% Flow rate calculation
+inputFlowRate = a(1,1,2); %Determines the inlet flow rate based off of the first element in the second 2D array of the inlet stream. 
+flowRate = nan(numOutMatrices,1); % Form a vector of NaN values (2,1).
+
+for i = 1:numOutMatrices % Loop from 1 to the final number of matrices 2.
+    flowRate(i) = inputFlowRate/numOutMatrices; % Determine the new flow rate of the outlet streams based of the total inlet flow rate and number of outlet matrices (two). Store the flow rate after each iteration. Assigns equal flow rate to each stream.
+end
+
+%% Constructing the pattern matrices
+cutA = imresize(a(:,1:resolution/numOutMatrices,1), [resolution, resolution],'nearest'); %Take the pattern matrix a(:,:,1) and extract all the rows, and those columns specified from to resolution/numOutMatrices. Resize this to the desired resolution based off the provided input variables.
+cutB = imresize(a(:,resolution/numOutMatrices+1:end,1), [resolution, resolution],'nearest'); %Take the pattern matrix a(:,:,1) and extract all the rows and those columns from one index higher than resolution/numOutMatrices to the end of the array. Resize this to the desired resolution based off the provided input variables.
+
+%% Predefining output variables
+outA = a; %Assigns the correct dimensions for the output variable.
+outB = a; %Assigns the correct dimensions for the output variable.
+
+outA(:,:,1) = cutA; %The new pattern of array A (outA(:,:,1)) after the horizontal splitting. 
+outA(:,:,2) = flowRate(1).*ones(resolution,resolution); %The new flow rate of array A (outA(:,:,2)) after the horizontal splitting. Calculates the new flow rate array using the individual flow rates (flowrate(1), first element in the flow rate vector) multiplied by a ones matrix of size based on the supplied resolution.
+outB(:,:,1) = cutB; %The new pattern of array B (outB(:,:,1)) after the horizontal splitting.
+outB(:,:,2) = flowRate(2).*ones(resolution,resolution); %The new flow rate of array B (outB(:,:,2)) after the horizontal splitting. Calculates the new flow rate array using the individual flow rates (flowrate(2), second element in the flow rate vector) multiplied by a ones matrix of size based on the supplied resolution.
+
+
+
+
+
+
+
